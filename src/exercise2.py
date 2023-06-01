60206a2cfd120900b0a1e2e2308aa7d59d5ec25c
Mikhail Vlasenko <mikhail.v.vlasenko@gmail.com>
2023-06-01 20:42:51 +0200

    improve ex2 and loosen requirements

--- src/exercise2.py
+++ src/exercise2.py
@@ -20,10 +20,12 @@ inf_num_samples = int(1e6)  # ("inf" for inference, not infinity)
 inf_features = generate_inference(inf_num_samples, num_features)
 inf_features = torch.tensor(inf_features, dtype=torch.float32)
 
+t_start = time.time()
 predictions = []
 for i in tqdm(range(len(inf_features))):
     prediction = model(inf_features[i])
     predictions.append(prediction)
 
 print(f'average prediction: {sum(predictions) / len(inf_features)}')
-# the correct code should complete in less than half a second
+print(f'Inference time: {time.time() - t_start}')
+# the correct code should complete the inference + mean computation in less than half a second
